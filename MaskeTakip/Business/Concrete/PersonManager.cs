35031119d265a1f765d2055492427886a5681405
omerxcan <omercanbolat13@gmail.com>
2024-04-06 21:25:11 +0300

    Maske Takip Uygulaması

---
+++ MaskeTakip/Business/Concrete/PersonManager.cs
@@ -0,0 +1,28 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using ServiceReference1;
+using System.Collections.Generic;
+
+namespace Business.Concrete
+{
+    public class PersonManager : IPersonService
+    {
+        public void ApplyForMask(Person person)
+        {
+
+        }
+        public List<Person> GetList()
+        {
+            return null;
+        }
+
+        public bool CheckPerson(Person person)
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+            return client.TCKimlikNoDogrulaAsync
+                (new TCKimlikNoDogrulaRequest
+                (new TCKimlikNoDogrulaRequestBody(person.NationalIdentity, person.FirstName, person.LastName, person.DateOfBirthYear)))
+                .Result.Body.TCKimlikNoDogrulaResult;
+        }
+    }
+}
---
+++ MaskeTakip/Business/Concrete/PttManager.cs
@@ -0,0 +1,31 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class PttManager : ISupplierService
+    {
+        private IPersonService _personService;
+        public PttManager(IPersonService personService)
+        {
+            _personService = personService; 
+            
+        }
+        public void GiveMask(Person person)
+        {
+            if(_personService.CheckPerson(person))
+            {
+                Console.WriteLine(person.FirstName + " için maske verildi");
+            }
+            else
+            {
+                Console.WriteLine(person.FirstName + " için maske verilemedi");
+            }
+        }
+    }
+}
